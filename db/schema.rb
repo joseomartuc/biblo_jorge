6591d85f12e314b76cc960baecb20a3190a5f4d7
unknown <Administrador@.(none)>
2010-06-30 19:50:03 -0300

    Scaffold Tag: cambios db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100630224313) do
+ActiveRecord::Schema.define(:version => 20100630224841) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(:version => 20100630224313) do
   create_table "books", :force => true do |t|
     t.string   "title"
     t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
